d1b9b94d89537ade3c1ea5048cba6c18655e7e4c
aukanslk <63707141+aukanslk@users.noreply.github.com>
2020-04-20 09:56:30 +0700

    rename and upload
    rename file and upload folder

--- Homework BKI IIN/assignment from  p'mint/Untitled12.sql
+++
@@ -1,7 +0,0 @@
-select distinct ag.agent_code,ag.bancass_flg
-     ,case when ag.agent_code = '6900' and ag.bancass_flg = 'BBL' then 'Counter'
-           when ag.agent_code = '6900' and ag.bancass_flg <> 'BBL' then 'No_counter'
-           else 'XX' end as grp
-           
-from agent_group ag
-where ag.agent_code = '6900' and ag.bancass_flg = 'BBL')
